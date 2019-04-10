157fb616e2649ce59ee0f7f99597856618a4f9ac
Suren Grigoryan <suren.grigoryan@gmail.com>
2019-04-10 17:51:09 +0200

    Added UserDao and AuthDao

--- app/src/main/java/org/freeandroidtools/trendinggithub/db/db.kt
+++ app/src/main/java/org/freeandroidtools/trendinggithub/db/db.kt
@@ -1,34 +1,106 @@
 package org.freeandroidtools.trendinggithub.db
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Single
+import org.freeandroidtools.trendinggithub.model.AuthorizedUser
 import org.freeandroidtools.trendinggithub.model.DateConverter
 import org.freeandroidtools.trendinggithub.model.GithubRepo
+import org.freeandroidtools.trendinggithub.model.StarredRepo
+import org.freeandroidtools.trendinggithub.model.User
 
+@Database(
+    entities = [StarredRepo::class, GithubRepo::class, AuthorizedUser::class, User::class],
+    exportSchema = false,
+    version = 8
+)
+@TypeConverters(DateConverter::class)
+abstract class RepoDatabase : RoomDatabase() {
+    abstract fun repoDao(): RepoDao
+    abstract fun starredReposDao(): StarredRepoDao
+    abstract fun authDao(): AuthDao
+    abstract fun userDao(): UserDao
+}
+
+@Dao
+interface StarredRepoDao {
+    @Insert
+    fun insert(repo: StarredRepo)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(repos: List<StarredRepo>)
+
+    @Query("DELETE FROM starredRepo")
+    fun deleteAll()
+
+    @Query("SELECT * FROM starredRepo WHERE id = :id")
+    fun getById(id: String): LiveData<List<StarredRepo>>
+
+    @Query("SELECT * FROM starredRepo")
+    fun getAll(): Flowable<List<StarredRepo>>
+}
 
 @Dao
 interface RepoDao {
 
-    @Query("SELECT * FROM repo")
-    fun getAll(): LiveData<List<GithubRepo>>
-
     @Insert
     fun insert(repo: GithubRepo)
 
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(repos: List<GithubRepo>)
-
 
     @Query("DELETE FROM repo")
     fun deleteAll()
 
     @Query("SELECT * FROM repo WHERE id = :id")
     fun getById(id: String): LiveData<List<GithubRepo>>
+
+    @Query("SELECT * FROM repo")
+    fun getAll(): Flowable<List<GithubRepo>>
 }
 
-@Database(entities = [(GithubRepo::class)], exportSchema = false, version = 6)
-@TypeConverters(DateConverter::class)
-abstract class RepoDatabase : RoomDatabase() {
-    abstract fun repoDao(): RepoDao
+@Dao
+interface AuthDao {
+    @Insert
+    fun insert(user: AuthorizedUser): Long
+
+    @Query("DELETE FROM AuthorizedUser")
+    fun deleteAll()
+
+    @Query("SELECT * FROM AuthorizedUser LIMIT 1")
+    fun getSelected(): Single<AuthorizedUser>
+}
+
+@Dao
+interface UserDao {
+    @Insert
+    fun insert(user: User): Long
+
+    @Query("SELECT * FROM user")
+    fun getAll(): LiveData<List<User>>
+
+    @Query("SELECT * FROM user")
+    fun getAllNonLive(): List<User>
+
+
+    @Query("SELECT * FROM user WHERE dbId= :id")
+    fun getById(id: Long): Maybe<List<User>>
+
+    @Delete
+    fun delete(user: User)
+
+    @Query("DELETE FROM user WHERE id= :id")
+    fun deleteById(id: Long)
+
+    @Query("DELETE FROM user")
+    fun deleteAll(): Int
 }
