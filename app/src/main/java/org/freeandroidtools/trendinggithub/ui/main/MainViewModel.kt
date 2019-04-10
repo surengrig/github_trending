f669caf3518cebcdb53db1ca1d6b8d385970c45b
Suren Grigoryan <suren.grigoryan@gmail.com>
2019-04-10 17:37:33 +0200

    Updated MainActivity to include user logout functionality, added VM

---
+++ app/src/main/java/org/freeandroidtools/trendinggithub/ui/main/MainViewModel.kt
@@ -0,0 +1,19 @@
+package org.freeandroidtools.trendinggithub.ui.main
+
+import androidx.lifecycle.ViewModel
+import org.freeandroidtools.trendinggithub.TrendingApp
+import org.freeandroidtools.trendinggithub.repository.GithubApiRepository
+import javax.inject.Inject
+
+class MainViewModel : ViewModel() {
+    init {
+        TrendingApp.appComponent.inject(this)
+    }
+
+    @Inject
+    lateinit var repository: GithubApiRepository
+
+    fun logout() {
+        repository.logout()
+    }
+}
